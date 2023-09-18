6b5350d5c079d664c24a6ffe168563aa19261359
devitoolz <tkdfuf0923@gmail.com>
2023-09-18 10:48:12 +0900

    feat: header 1차 작업

---
+++ components/common/Header.tsx
@@ -0,0 +1,23 @@
+import styles from '@/styles/header.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles['logo-container']}>
+        <Link href="/">
+          <Image
+            className={styles.logo}
+            src="/logo.png"
+            alt="건강증진센터"
+            width={40}
+            height={40}
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
