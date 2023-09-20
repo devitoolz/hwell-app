c7e3d8f663b0a585df480f1816fa6b1376afbdd4
devitoolz <tkdfuf0923@gmail.com>
2023-09-20 10:21:58 +0900

    feat: SEO 세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  i18n: {
+    /** https://nextjs.org/docs/advanced-features/i18n-routing#getting-started */
+    locales: ['ko'],
+    defaultLocale: 'ko',
+  },
 };
 
 module.exports = nextConfig;
